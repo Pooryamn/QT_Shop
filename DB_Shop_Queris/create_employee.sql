a637cbf8ff0da2ecafcc628f01c8c7b1f86d69bf
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-03-08 16:07:22 +0330

    create employee table query added

---
+++ DB_Shop_Queris/create_employee.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE Employee (
+	"Employee ID"	SERIAL,
+	Firstname	varchar(50) not null,
+	Lastname	varchar(50) not null,
+	"National ID"	varchar(12) not null,
+	Email		varchar(70) not null,
+	"Phone number"	varchar(15),
+	"Mobile number"	varchar(20) not null,
+	Address		text,
+	city		varchar(30),
+	"Postal code"	varchar(15),
+	birthdate	date,
+	Attachments	bytea,
+	Username	varchar(40) not null,
+
+	primary key("Employee ID") 
+);
