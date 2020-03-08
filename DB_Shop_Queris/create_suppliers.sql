ef6b675c5516306e61878891b6150cfe543ef552
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-03-08 16:20:21 +0330

    create suppliers table query added

---
+++ DB_Shop_Queris/create_suppliers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Suppliers(
+	"Supplier ID"	SERIAL,
+	"Company name"	varchar(30) not null,
+	Email 		varchar(70) not null,
+	Phone 		varchar(20),
+	"Company Type"	varchar(30) not null,
+	Attachments	bytea,
+
+	primary key("Supplier ID") 
+);
