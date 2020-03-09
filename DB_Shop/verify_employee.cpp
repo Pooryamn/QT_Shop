18d73126d1f22f6e3c380ac7cee0a757871eed93
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-03-10 02:04:07 +0330

    two set and get method added to Verify_employee class to communicate with SignUP class

--- DB_Shop/verify_employee.cpp
+++ DB_Shop/verify_employee.cpp
@@ -6,9 +6,34 @@ verify_employee::verify_employee(QWidget *parent) :
     ui(new Ui::verify_employee)
 {
     ui->setupUi(this);
+    status = false;
 }
 
 verify_employee::~verify_employee()
 {
     delete ui;
+}
+
+void verify_employee::SetPass(QString emp,QString mas){
+    Employee = emp;
+    Master = mas;
+}
+
+void verify_employee::on_btn_edit_clicked()
+{
+    this->close();
+}
+
+void verify_employee::on_btn_verify_clicked()
+{
+    if((ui->txt_verifyCode->text() == Employee) && (ui->txt_mastercode->text() == Master)){
+        status = true;
+    }
+    else {
+        status = false;
+    }
+}
+
+bool verify_employee::get_status(){
+    return status;
 }
