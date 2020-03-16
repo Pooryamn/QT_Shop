7f347cada3c064ebc300f3d0dac1dc511595842a
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-03-16 23:12:09 +0330

    Database.h documented

--- DB_Shop/database.h
+++ DB_Shop/database.h
@@ -7,22 +7,20 @@
 class Database
 {
 public:
+
     Database();
-    int Connect();
-    void Disconnect();
-    bool Execute(QString& qry,QSqlQuery& query);
-    bool IsConnected();
+    int Connect(); // try to connect and return status as int
+    void Disconnect(); // Disconnet Object from database
+    bool Execute(QString& qry,QSqlQuery& query); // get a valid query and try to execute it. if executing was successful return true otherwise return false
+    bool IsConnected(); // check that object is connected to databae or not
+
 private:
 
     QSqlDatabase DB;
+    QString error; // store last error from database
+    bool Connection_Status; // if connection is ok(true) otherwise (false)
+    bool Create_DB(); // create database tables
 
-    QString error;
-
-    bool Connection_Status;
-
-    bool Create_DB();
-
-    bool New_DB();
 };
 
 #endif // DATABASE_H
