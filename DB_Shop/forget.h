522680d91956c8479f4c09977e0ffc43b2b26978
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-03-16 23:32:23 +0330

    forget.h documented

--- DB_Shop/forget.h
+++ DB_Shop/forget.h
@@ -25,13 +25,16 @@ private slots:
     void on_btn_recover_clicked();
 
 private:
+
     Ui::forget *ui;
-    Database DB;
-    QString find_email(QString username,int type);
-    QString find_password(QString username,int& type);
-    bool Send_Email(QString Email,QString Name,QString Subject,QString Body);
-    bool is_valid_User();
-    int Type;
+    Database DB; // database object
+    int Type; // Type of user( 1 = Normal User, 2 = Employee , 3 = master)
+
+    QString find_email(QString username,int type); // gets the type and username and return email by searchin in Database
+    QString find_password(QString username,int& type); // gets username and type and return password
+    bool Send_Email(QString Email,QString Name,QString Subject,QString Body);// send an simple text email to user (contains password of user)
+    bool is_valid_User(); // check that Username in ui is valid or not(is it in database or not?)
+
 };
 
 #endif // FORGET_H
