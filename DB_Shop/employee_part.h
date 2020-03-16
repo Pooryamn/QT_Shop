d6d9b54f683c5d2f6eed105709b18cccdb10b116
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-03-16 23:21:54 +0330

    Employee_part.h documented

--- DB_Shop/employee_part.h
+++ DB_Shop/employee_part.h
@@ -1,8 +1,10 @@
 #ifndef EMPLOYEE_PART_H
 #define EMPLOYEE_PART_H
 
+// QT headers :
 #include <QDialog>
 
+// My headers :
 #include "new_stock.h"
 #include "user_edit.h"
 #include "user_payment.h"
@@ -50,18 +52,24 @@ private slots:
 
 private:
 
+    //vars :
     Ui::employee_part *ui;
-    QString UserName;
-    Database DB;
-    void set_form();
-    void load_data(QString username);
-    void Search(QString key,int Type);
-    void load_search_data(int ID);
-    QString find_company(int id);
-    QByteArray image[6];
-    void Setup();
+    QString UserName; // Employee username for using in functions
+    Database DB; // Database Object
+    QByteArray image[6]; //Array of Bytearrays for storing
+    QSqlQueryModel* model; // Model of Sql Query results
 
-    QSqlQueryModel* model;
+    // funcs :
+    void load_data(QString username); // load Employee information in ui form
+
+    // Search Products with a search key and type(0 = search by name , 1 = search by category)
+    void Search(QString key,int Type);
+    void load_search_data(int ID);// load product info + pics in ui form
+    QString find_company(int id); // gets Company ID and return name of company
+    void Setup(); // set basics settings for ui form(disabling object , clear pics)
+    void set_form(); // set some rule for objets in ui
+
+
 };
 
 #endif // EMPLOYEE_PART_H
