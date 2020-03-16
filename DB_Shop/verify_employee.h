9936e83c55ed6672d20f0257f38de1ab955c4623
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-03-17 00:35:36 +0330

    verifing_employee.h documented

--- DB_Shop/verify_employee.h
+++ DB_Shop/verify_employee.h
@@ -14,8 +14,8 @@ class verify_employee : public QDialog
 
 public:
     explicit verify_employee(QWidget *parent = nullptr);
-    void SetPass(QString emp,QString mas);
-    bool get_status();
+    void SetPass(QString emp,QString mas); // employee code and master code
+    bool get_status(); // true -> if codes confrim successfully
     ~verify_employee();
 
 private slots:
@@ -25,9 +25,9 @@ private slots:
 
 private:
     Ui::verify_employee *ui;
-    QString Employee;
-    QString Master;
-    bool status;
+    QString Employee;//employee confrom code
+    QString Master; // master confrom code
+    bool status; // true -> if codes confrim successfully
 };
 
 #endif // VERIFY_EMPLOYEE_H
